518476c930f3b17b56f6ebbfde3bd424e1c78752
tunzy-shop <tunzyprivate@gmail.com>
2025-11-30 10:06:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  ownerNumber: '+2349067345425',      // owner phone (with +)
-  ownerName: 'Tunzy Shop',            // owner display name
-  botName: 'TUNZY MD V1',             // bot name
-  channelLink: 'https://whatsapp.com/channel/0029Vb65QAGGOj9nnQynhh04',
-  groupLink: 'https://chat.whatsapp.com/IaZpA3r6fgYIqMXZkWSVNd',
-  prefix: '.',                        // command prefix
-  sessionFolder: './tmp',             // session folder path
-  mediaFolder: './media'              // folder with botpic.jpeg
-};
+    BOT_NAME: "TUNZY MD BOT",
+    OWNER_NUMBER: "2349067345425",
+    GROUP_LINK: "https://chat.whatsapp.com/IaZpA3r6fgYIqMXZkWSVNd",
+    CHANNEL_LINK: "https://whatsapp.com/channel/0029Vb65QAGGOj9nnQynhh04",
+    MENU_PIC: "./media/botpic.jpeg",
+    ANTI_LINK: {
+        ON: true,
+        KICK: true,
+        WARN: 3,
+        DELETE: true
+    }
+}
