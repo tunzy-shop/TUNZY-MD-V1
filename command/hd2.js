51264251ff8b043d5e3a61407f61d37c963c4432
tunzy-shop <tunzyprivate@gmail.com>
2025-11-30 17:13:00 +0100

    Update hd2.js

--- command/hd2.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: "hd2",
-  run: async (sock, msg) => {
-    await sock.sendMessage(msg.chat, { text: "HD2 works same as .hd. Reply to a video or provide link." });
-  }
-};
