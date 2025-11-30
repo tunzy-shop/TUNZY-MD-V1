d69451b5fc7128d83bf85f6e3bf6e44b3c771cfd
tunzy-shop <tunzyprivate@gmail.com>
2025-11-30 17:38:52 +0100

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,19 @@
+const settings = {
+  packname: 'TUNZY MD BOT',
+  author: 'TUNZY',
+  botName: "TUNZY MD BOT",
+  botOwner: 'TUNZY', // Your name
+  ownerNumber: '2349067345425', // Your WhatsApp number with country code, no +
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', // You can keep or replace
+  commandMode: "public", // public or private mode
+  maxStoreMessages: 50, // how many messages to keep in memory for features
+  storeWriteInterval: 10000, // interval to save store to file
+  description: "TUNZY MD BOT - Manage groups & automate tasks",
+  version: "1.0.0",
+  menuPic: "./media/botpic.jpeg", // path to menu image
+  groupLink: "https://chat.whatsapp.com/IaZpA3r6fgYIqMXZkWSVNd",
+  channelLink: "https://whatsapp.com/channel/0029Vb65QAGGOj9nnQynhh04",
+  updateZipUrl: "https://github.com/tunzy-shop/render-pairing/archive/refs/heads/main.zip", // optional updates
+};
+
+module.exports = settings;
