d84234218d37c722ad092421dfe8b4cc3ad23678
tunzy-shop <tunzyprivate@gmail.com>
2025-12-10 20:40:48 +0100

    Delete command directory

--- command/acceptall.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "acceptall",
-    cmd: ['acceptall'],
-    run: async (sock, msg) => {
-        // placeholder: depends on your logic
-        await sock.sendMessage(msg.key.remoteJid, { text: "All requests accepted." });
-    }
-};
--- command/restart.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "restart",
-    cmd: ['restart'],
-    run: async (sock, msg) => {
-        await sock.sendMessage(msg.key.remoteJid, { text: "♻ Restarting bot..." });
-        process.exit(1);
-    }
-};
